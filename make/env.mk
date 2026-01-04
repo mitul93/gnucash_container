be14bbc389c2e0815da3652c2ec581f8aea770ae
Mitul Vekariya <vekariya93@gmail.com>
2026-01-04 18:40:31 +0100

    Add prefix DOCKER to USERNAME, USER_UID, USER_GID

--- make/env.mk
+++ make/env.mk
@@ -5,6 +5,6 @@ DOCKER_IMAGE_NAME:=gnucash_docker
 DOCKER_BASE_IMAGE_NAME:=ubuntu
 DOCKER_BASE_IMAGE_TAG:=24.04
 
-DOCKER_USERNAME=$(shell whoami)
-DOCKER_UID=$(shell id -u)
-DOCKER_GID=$(shell id -g)
+DOCKER_USERNAME=gnucash
+DOCKER_USER_UID=$(shell id -u)
+DOCKER_USER_GID=$(shell id -g)
